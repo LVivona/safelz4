a1220b7c1672a71e0249212d715ef8db759747f4
Luca Vivona <lucavivona01@gmail.com>
2025-05-23 18:15:51 -0400

    chore: add block prepend with dict de/compression

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,13 +1,14 @@
 import os
 from safelz4.block import (
     compress,
-    compress_str_utf8,
     compress_into,
-    compress_utf8_prepend_size,
     compress_prepend_size,
+    compress_prepend_size_with_dict,
+    compress_with_dict,
     decompress,
     decompress_into,
-    decompress_size_prepended,
+    decompress_with_dict,
+    decompress_prepend_size_with_dict,
     get_maximum_output_size,
 )
 
@@ -24,9 +25,9 @@ def test_empty_block_compression():
 
 def test_regular_block_compress():
 
-    with open(FILE_1Kb, "r") as f:
+    with open(FILE_1Kb, "rb") as f:
         output = f.read(-1)
-        output_c = compress(output.encode("utf-8"))
+        output_c = compress(output)
 
         max_possible_size = get_maximum_output_size(len(output))
         reduc = 1 - (len(output_c) / len(output))
@@ -36,35 +37,9 @@ def test_regular_block_compress():
         assert max_possible_size >= len(output_c)
 
 
-def test_str_utf8_into_compress():
-    with open(FILE_1Kb, "r") as f:
-        output = f.read(-1)
-        buffer = compress_str_utf8(output)
-
-        max_size = get_maximum_output_size(len(output))
-        reduc = 1 - (len(buffer) / len(output))
-        # TEST
-        # print(f"total reduction: {reduc:.2f}%")
-        assert reduc <= 1.0
-        assert max_size >= len(buffer)
-
-
-def test_compress_utf8_prepend_size():
-    with open(FILE_1Kb, "r") as f:
-        output = f.read(-1)
-        buffer = compress_utf8_prepend_size(output)
-
-        max_size = get_maximum_output_size(len(output))
-        reduc = 1 - (len(buffer) / len(output))
-        # TEST
-        # print(f"total reduction: {reduc:.2f}%")
-        assert reduc <= 1.0
-        assert max_size >= len(buffer)
-
-
 def test_compression_into():
-    with open(FILE_1Kb, "r") as f:
-        stdin = f.read(-1).encode("utf-8")
+    with open(FILE_1Kb, "rb") as f:
+        stdin = f.read(-1)
         max_size = get_maximum_output_size(len(stdin))
 
         buffer = bytearray(max_size)
@@ -78,8 +53,8 @@ def test_compression_into():
 
 
 def test_compress_prepend_size():
-    with open(FILE_1Kb, "r") as f:
-        stdin = f.read(-1).encode("utf-8")
+    with open(FILE_1Kb, "rb") as f:
+        stdin = f.read(-1)
         max_size = get_maximum_output_size(len(stdin))
 
         output = compress_prepend_size(stdin)
@@ -90,10 +65,6 @@ def test_compress_prepend_size():
         assert max_size >= len(output)
 
 
-# # ..End of Compression..
-
-
-# # ..Begining of Decompression..
 def test_regular_decompression_empty():
     empty_string = b""
     buffer = compress(empty_string)
@@ -103,8 +74,8 @@ def test_regular_decompression_empty():
 
 def test_regular_decompression():
 
-    with open(FILE_1Kb, "r") as file:
-        stream_f = file.read(-1).encode("utf-8")
+    with open(FILE_1Kb, "rb") as file:
+        stream_f = file.read(-1)
 
         cbuffer_f = compress(stream_f)
 
@@ -113,8 +84,8 @@ def test_regular_decompression():
 
 
 def test_decompress_into():
-    with open(FILE_1Kb, "r") as file:
-        stream_f = file.read(-1).encode("utf-8")
+    with open(FILE_1Kb, "rb") as file:
+        stream_f = file.read(-1)
 
         cbuffer = compress(stream_f)
 
@@ -125,8 +96,53 @@ def test_decompress_into():
 
 
 def test_decompress_size_prepended():
-    with open(FILE_1Kb, "r") as file:
-        stream_f = file.read(-1).encode("utf-8")
-        cbuffer = compress_prepend_size(stream_f)
-        output = decompress_size_prepended(cbuffer)
-        assert output == stream_f
+    with open(FILE_1Kb, "rb") as file:
+        stdin = file.read(-1)
+        buffer = compress_prepend_size(stdin)
+
+        # block size should be 32 unsigned integer
+        # \x00\x00\x00\x00
+        content_size = buffer[:4]
+        size = int.from_bytes(content_size, "little")
+        output = decompress(buffer[4:], size)
+
+        assert output == stdin
+
+
+def test_compression_with_dict():
+    ext_dict = b"\x00\x00\x00\00"
+    with open(FILE_1Kb, "rb") as file:
+        stdin = file.read(-1)
+        buffer = compress_with_dict(stdin, ext_dict)
+
+        output = decompress_with_dict(buffer, len(stdin), ext_dict)
+        assert output == stdin
+
+
+def test_compression_prepend_size_with_dict():
+    ext_dict = b"\x00\x00\x00\00"
+    with open(FILE_1Kb, "rb") as file:
+        expected = file.read(-1)
+        buffer = compress_prepend_size_with_dict(expected, ext_dict)
+
+        min_size = int.from_bytes(buffer[:4], "little")
+        output = decompress_with_dict(
+            buffer[4:],
+            min_size,
+            ext_dict
+        )
+        assert min_size == len(expected)
+        assert expected == output
+
+
+def test_decompression_prepend_size_with_dict():
+    ext_dict = b"\x00\x00\x00\00"
+    with open(FILE_1Kb, "rb") as file:
+        expected = file.read(-1)
+        buffer = compress_prepend_size_with_dict(expected, ext_dict)
+
+        output = decompress_prepend_size_with_dict(
+            buffer,
+            ext_dict
+        )
+        assert expected == output
